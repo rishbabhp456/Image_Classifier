e46160bf96b9288a2a4fb867e1892c6422414e9f
rishabhp456 <patilrisshabh22@gmail.com>
2026-07-14 22:21:48 +0530

    updated connection string CosmosDB

--- config.py
+++ config.py
@@ -3,7 +3,8 @@ import os
 FLASK_HOST = '0.0.0.0'
 FLASK_PORT = 8000
 
-MONGO_URL = 'mongodb://localhost:27017/'
+MONGO_PASSWORD = 'test123$'
+MONGO_URL = f'mongodb+srv://rishabhp:{MONGO_PASSWORD}@docdb-cluster-20260630-0923.global.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000'
 db_name = 'image_clf_db'
 collection_data1 = 'rice_img_clf'
 collection_data2 = 'vehicle_img_clf'
