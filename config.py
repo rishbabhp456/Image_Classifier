710c7a6afa6d8c657943fefd717983de1fbc8251
rishabhp456 <patilrisshabh22@gmail.com>
2026-07-15 09:05:50 +0530

    updated config absolute filepath

--- config.py
+++ config.py
@@ -10,10 +10,11 @@ collection_data1 = 'rice_img_clf'
 collection_data2 = 'vehicle_img_clf'
 collection_user = 'clf_users'
 
+# --- FIX: Use the file's absolute directory path instead of os.getcwd() ---
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-MODEL_PATH = os.path.join(os.getcwd(), 'artifacts', 'rice_classifier_vgg16.keras')
-CLASS_NAMES_PATH = os.path.join(os.getcwd(), 'artifacts', 'class_names.json')
+MODEL_PATH = os.path.join(BASE_DIR, 'artifacts', 'rice_classifier_vgg16.keras')
+CLASS_NAMES_PATH = os.path.join(BASE_DIR, 'artifacts', 'class_names.json')
 
-MODEL2_PATH = os.path.join(os.getcwd(), 'artifacts', 'car_bike_classifier_vgg16.keras')
-CLASS_NAMES2_PATH = os.path.join(os.getcwd(), 'artifacts', 'car_bike_class_names.json')
-
+MODEL2_PATH = os.path.join(BASE_DIR, 'artifacts', 'car_bike_classifier_vgg16.keras')
+CLASS_NAMES2_PATH = os.path.join(BASE_DIR, 'artifacts', 'car_bike_class_names.json')
